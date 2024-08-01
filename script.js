9c9c1eaa7f57ee7721deedc3cb6106000097906a
akyadav4710 <163339150+akyadav4710@users.noreply.github.com>
2024-08-01 15:47:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function indexOfIgnoreCase(str, subStr) {
-  // write your code hereconst lowerStr = str.toLowerCase();
+    // Convert both strings to lowercase for case-insensitive comparison
+    const lowerStr = str.toLowerCase();
     const lowerSubStr = subStr.toLowerCase();
 
     // Find the index of the first occurrence of lowerSubStr in lowerStr
     return lowerStr.indexOf(lowerSubStr);
-} 
+}
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
